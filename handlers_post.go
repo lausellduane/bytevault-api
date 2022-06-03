607c920a78be4934400c14d6cccbd9435aa7c1a5
lausellduane <lausellduane@gmail.com>
2022-06-02 20:19:41 -0400

    feat: add postCodefragmentHandler
    This commit add a handler to write new code fragments into the database.

--- handlers_post.go
+++ handlers_post.go
@@ -1,48 +1,40 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"encoding/json"
-// 	// "errors"
-// 	"net/http"
-// 	// "strconv"
-// 	// "strings"
-// 	// "context"
-// 	"io/ioutil"
-// 	"go.mongodb.org/mongo-driver/bson"
-// )
+import (
+	"fmt"
+	"encoding/json"
+	"net/http"
+	"io/ioutil"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
-// func postCodeFragmentHandler(w http.ResponseWriter, r *http.Request){
-// 	fmt.Println("postCodeFragmentHandler")
-// 	fmt.Println("r.Body: ", r.Body)
-// 	bodyData, err := ioutil.ReadAll(r.Body)
-// 	fmt.Println("bodyData: ", string(bodyData))
-// 	if err != nil{
-// 		fmt.Sprintf("%v", err)
-// 	}
-// 	// var body map[string]interface{}
-// 	var codefragment CodeFragment
-// 	// if err := json.Unmarshal(bodyData, &body); err != nil {
-//     //     fmt.Sprintf("json unmarshal err: %v", err)
-//     // }
-// 	if err := json.Unmarshal(bodyData, &codefragment); err != nil {
-// 		fmt.Println("json unmarshal err: ", err)
-// 		http.Error(w, http.StatusText(500), 500)
-// 		return
-//     }
-//     fmt.Println("codefragment: ", codefragment)
-// 	fmt.Println("codefragment.Title: ", codefragment.Title)
-// 	// fmt.Println("codefragment.Title string: ", string(codefragment.Title))
-// 	// fmt.Println("body: ", body)
+func postCodeFragmentHandler(w http.ResponseWriter, r *http.Request){
+	collection := db.Collection("fragments")
+	// Parse body
+	bodyData, err := ioutil.ReadAll(r.Body)
+	if err != nil{
+		http.Error(w, fmt.Sprintf("%v", err), 500)
+		return
+	}
+	var fragment CodeFragment
+	if err := json.Unmarshal(bodyData, &fragment); err != nil {
+		http.Error(w, fmt.Sprintf("%v", err), 500)
+		return
+    }
 
-// 	codeFragmentResult, err := init().Database("byteVaultDB").database.Collection("codefragments").InsertOne(ctx, bson.M{
-// 		"_id": codefragment.ID, "title": codefragment.Title, "description": codefragment.Description,
-// 		"notes": codefragment.Notes, "value": codefragment.Value, "tags": codefragment.Tags,
-// 		"language": codefragment.Language})
-// 	if err != nil{
-// 		fmt.Println("codeFragmentResult err: ", err)
-// 		http.Error(w, http.StatusText(500), 500)
-// 		return
-// 	}
-// 	fmt.Println("codeFragmentResult: ", codeFragmentResult)
-// }
+	fragmentResult, err := collection.InsertOne(ctx, bson.D{
+		{"title", fragment.Title},
+		{"description", fragment.Description},
+		{"notes", fragment.Notes},
+		{"value", fragment.Value},
+		{"tags", fragment.Tags},
+		{"language", fragment.Language},
+	})
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("InsertOne() ERROR: %s", err), 500)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(fragmentResult)
+}
